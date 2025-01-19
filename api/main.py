2581758c06ff8b66132aec703b5741e1f3f3d7e3
diegodio2712 <145599668+diegodio2712@users.noreply.github.com>
2025-01-19 16:32:08 -0300

    Add files via upload

---
+++ api/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+vendas = {
+    1: {"item": "lata", "preco_unitario": 4, "quantidade": 5},
+    2: {"item": "garrafa 2L", "preco_unitario": 15, "quantidade": 5},
+    3: {"item": "garrafa 750ml", "preco_unitario": 10, "quantidade": 5},
+    4: {"item": "lata mini", "preco_unitario": 2, "quantidade": 5},
+}
+
+@app.get("/")
+async def home():
+    return {"Vendas": len(vendas)}
+
+@app.get("/vendas/{id_venda}")
+async def pegar_venda(id_venda:int):
+    return vendas[id_venda]
